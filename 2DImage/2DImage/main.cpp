4b992d19ae13712626ca0c4a3c71c81f19bd2182
brianmurii <72038112+brianmurii@users.noreply.github.com>
2021-11-07 18:15:24 +0300

    Addition of windows and door

--- 2DImage/2DImage/main.cpp
+++ 2DImage/2DImage/main.cpp
@@ -23,20 +23,70 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        /*glClear(GL_COLOR_BUFFER_BIT); */
-        glBegin(GL_POLYGON);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(1, 1, 1, 1);
 
+        //Outer frame
+        glBegin(GL_LINE_LOOP);
+        glColor3f(0, 0, 0);
         glVertex2f(-0.7, 0.7);
         glVertex2f(0.5, 0.7);
         glVertex2f(0.5, 0.5);
         glVertex2f(0.4, 0.5);
         glVertex2f(0.4, -0.3);
-        glVertex2f(-0.7, -0.3);
+        glVertex2f(-0.6, -0.3);
         glVertex2f(-0.6, 0.5);
         glVertex2f(-0.8, 0.5);
         glVertex2f(-0.8, 0.7);
         
         glEnd();
+
+
+        //window on the left
+        glBegin(GL_LINE_LOOP);
+        glColor3f(0, 0, 0);
+        glVertex2f(-0.5f, 0.3f);
+        glVertex2f(-0.3f, 0.3f);
+        glVertex2f(-0.3f, 0.5f);
+        glVertex2f(-0.5f, 0.5);
+        glEnd();
+
+        glBegin(GL_LINES);
+        glColor3f(0, 0, 0);
+        glVertex2f(-0.5f, 0.4f);
+        glVertex2f(-0.3f, 0.4f);
+        glVertex2f(-0.4f, 0.3f);
+        glVertex2f(-0.4f, 0.5f);
+        glEnd();
+
+
+
+        //window on the right
+        glBegin(GL_LINE_LOOP);
+        glColor3f(0, 0, 0);
+        glVertex2f(0.1f, 0.3f);
+        glVertex2f(0.3f, 0.3f);
+        glVertex2f(0.3f, 0.5f);
+        glVertex2f(0.1f, 0.5f);
+        glEnd();
+
+        glBegin(GL_LINES);
+        glColor3f(0, 0, 0);
+        glVertex2f(0.1f, 0.4f);
+        glVertex2f(0.3f, 0.4f);
+        glVertex2f(0.2f, 0.3f);
+        glVertex2f(0.2f, 0.5f);
+        glEnd();
+
+        //door
+        glBegin(GL_LINE_LOOP);
+        glColor3f(0, 0, 0);
+        glVertex2f(-0.2f, -0.3f);
+        glVertex2f(0.0f, -0.3f);
+        glVertex2f(0.0f, 0.1f);
+        glVertex2f(-0.2f, 0.1f);
+        glEnd();
+
 
 
         /* Swap front and back buffers */
